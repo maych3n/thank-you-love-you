b4d6e155c1548f4c0785ace49e902b613b162806
maych3n <cheny197@newschool.edu>
2023-05-15 01:39:36 -0400

    envelopes

--- time.js
+++ time.js
@@ -52,3 +52,56 @@ fetch('notes.json')
         // }
       })
     });
+
+    // Sample JSON data
+const notesData = [
+  { language: "Quality Time", category: "A" },
+  // { name: "Item 2", category: "B" },
+  // { name: "Item 3", category: "A" },
+  // { name: "Item 4", category: "C" },
+  // { name: "Item 5", category: "B" }
+];
+
+// Function to filter JSON data by category
+function filterByCategory(data, category) {
+  return data.filter(item => item.category === category);
+}
+
+// Usage example
+const category = "A";
+const filteredData = filterByCategory(jsonData, category);
+console.log(filteredData);
+
+    // console.log(json)
+    // json.forEach(row => {
+    //   if(row.language="Quality Time"){notes.push(row)} 
+    
+    
+      
+    // });
+
+    
+
+
+
+//     fetch('time.json')
+//   .then(response => response.json())
+//   .then((data) => {
+//     // let parentElement = document.getElementById('background')
+
+//     for(let i = 0; i < data.length; i++) {
+//     //  Only show data with a grade of A and cuisine_description containing "pizza"
+//     if(data[i].language == "Time") {
+// //       let newDiv = document.createElement ('div')
+// //         newDiv.innerHTML =` 
+// //           <h4>${data[i].grade}</h4>
+// //           <p>${data[i].boro}</p>
+// //          <p>${data[i].street}</p>
+// //           <p>${data[i].zipcode}</p>
+// //          <p>${data[i].dba}</p>
+// // <p>${data[i].cuisine_description}</p>
+// //           `
+// //   //  parentElement.append(newDiv)
+//    }
+//   }
+// });
